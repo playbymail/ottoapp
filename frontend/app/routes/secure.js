f1a86454ffa1f74c8e580ecf21addfef142b2085
Michael Henderson <mdhender@mdhender.com>
2025-10-27 21:24:33 -0500

    Protect secure with a simple gate.

--- frontend/app/routes/secure.js
+++ frontend/app/routes/secure.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class SecureRoute extends Route {}
+export default class SecureRoute extends Route {
+  @service session;
+  beforeModel() {
+    if (!this.session.isAuthenticated) this.transitionTo('login');
+  }
+}
